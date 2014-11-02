2fc18764b3664bba3a26b81662fb8b3ff06853ac
Andy Bryant <ar.bryant@btinternet.com>
2014-11-02 01:39:02 +0000

    Move the superclass spec out of the spec for no superclass

--- spec/Base_spec.js
+++ spec/Base_spec.js
@@ -1,6 +1,6 @@
 describe('Base', function() {
 
-  describe('Ability to create classes', function() {
+  describe('Ability to create classes without superclasses', function() {
 
     it('should create classes from prototypes', function() {
       var prototype = {
@@ -26,33 +26,34 @@ describe('Base', function() {
       expect(instance.getName()).toEqual('Andy Bryant');
     });
     
-    describe('Ability to create classes with superclasses', function() {
+;
 
-      describe('given a parent class', function () {
+  });
+  describe('Ability to create classes with superclasses', function() {
 
-        it('should create a class which inherits methods from the parent', function () {
-          var parentPrototype = {
-            parentMethod: function () {
-            }
-          };
-          var ParentClass = Base.createClass(parentPrototype);
-          var ChildClass = Base.createClass(ParentClass, {});
-          var instance = ChildClass.createInstance();
-          var prototype = Object.getPrototypeOf(instance);
-          expect(Object.getPrototypeOf(prototype).parentMethod).toBe(parentPrototype.parentMethod);
-        });
+    describe('given a parent class', function () {
 
-        it('should create a class which has the child as its prototype', function() {
-          var prototype = {
-            childMethod: function () {
-            }
-          };
-          var ChildClass = Base.createClass({}, prototype);
-          var instance = ChildClass.createInstance();
-          expect(Object.getPrototypeOf(instance).childMethod).toBe(prototype.childMethod);
-        });
+      it('should create a class which inherits methods from the parent', function () {
+        var parentPrototype = {
+          parentMethod: function () {
+          }
+        };
+        var ParentClass = Base.createClass(parentPrototype);
+        var ChildClass = Base.createClass(ParentClass, {});
+        var instance = ChildClass.createInstance();
+        var prototype = Object.getPrototypeOf(instance);
+        expect(Object.getPrototypeOf(prototype).parentMethod).toBe(parentPrototype.parentMethod);
       });
 
+      it('should create a class which has the child as its prototype', function() {
+        var prototype = {
+          childMethod: function () {
+          }
+        };
+        var ChildClass = Base.createClass({}, prototype);
+        var instance = ChildClass.createInstance();
+        expect(Object.getPrototypeOf(instance).childMethod).toBe(prototype.childMethod);
+      });
     });
 
   });
